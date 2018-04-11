67f46081425cdd839a9fc99a024f49dd89506990
ymohajer <ymohajer@uci.edu>
2018-04-11 15:41:04 -0700

    debug model

--- frontlearn_unet.py
+++ frontlearn_unet.py
@@ -15,18 +15,19 @@ import keras.models as km
 def unet_model(channels,height,width):
 
     #-- define input
-    inputs = kl.Input(channels,height,width)
+    inputs = kl.Input((height,width,channels)
 
     #-- 1st convlution layer
-    c1 = kl.conv2D(12,3,3,activation='relu',padding='valid')(inputs)
-    print 'c1 shape: ', c1.shape
+    #-- c1 has shape (None, w, h, 12), 12 convlution outputs for 12 filters
+    c1 = kl.Conv2D(12,3,activation='relu',padding='same')(inputs)
+
     #-- pool, 2x2 blockcs
+    #-- p1 has shape (None, w/2, h/2, 12)
     p1 = kl.MaxPooling2D(pool_size=(2,2))(c1)
-    print 'p1 shaoe: ', p1.shape
 
     # 2nd convlution layer
-    c2 = kl.conv2D(24,3,3,activation='relu',padding='valid')(p1)
-    print 'c2 shape: ', c2.shape
+    #-- c2 has shape (None, w/2, h/2, 24)
+    c2 = kl.Conv2D(24,3,activation='relu',padding='same')(p1)
 
     #---------------------------------------------
     #-- now go back up to reconsturct the image
@@ -34,15 +35,20 @@ def unet_model(channels,height,width):
     #-- concatenate the 1st convolution layer with an upsampled 2nd layer
     #-- where the missing elements in the 2nd layer are padded with 0
     #-- concatenating along the color channels
-    up1 = kl.concatenate(axis=1)([kl.UpSampling2D(size=(2,2),c2),c1])
+    #-- upsampled_c2 has shape (None, w, h ,24)
+    upsampled_c2 = kl.UpSampling2D(size=(2,2))(c2)
+    #-- up1 has shape (None, w, h , 36)  (12+24 channels from c2 and c1)
+    up1 = kl.concatenate([upsampled_c2,c1],axis=3)
     #-- now do a convlution with the merged upsampled layer
-    c4 = kl.conv2D(12,3,3,activation='relu',padding='valid')(up1)
+    #-- c4 has shape (None, w, h , 12)
+    c4 = kl.Conv2D(12,3,activation='relu',padding='same')(up1)
 
     #-- convlution across the last 12 filters into 3 channels
-    c5 = kl.conv2D(3,3,3,activation='relu',padding='valid')(c4)
+    #-- c45has shape (None, w, h , 3)
+    c5 = kl.Conv2D(3,3,activation='relu',padding='same')(c4)
 
-    #-- do one final sigmoid convolution into just 1 final channel
-    c6 = kl.conv2D(1,1,1,activation='sigmoid')(c5)
+    #-- do one final sigmoid convolution into just 1 final channel (None, w, h , 1)
+    c6 = kl.Conv2D(1,1,activation='sigmoid')(c5)
 
     #-- make model
     model = km.Model(input=inputs,output=c6)
