b88d753e48ffb1cb6c18a335d4f4297c4b84ca6d
ymohajer <ymohajer@uci.edu>
2018-04-12 16:29:58 -0700

    train cnn , write weights to file
    Output is all white at this point. Need to work on it.

--- frontlearn_train.py
+++ frontlearn_train.py
@@ -11,42 +11,102 @@ Update History
 import os
 import numpy as np
 import keras
+from keras.preprocessing import image
 import imp
+import sys
+from glob import glob
+from PIL import Image
+import matplotlib.pyplot as plt
 
 #-- directory setup
 #- current directory
 ddir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'data'))
-img_dir = os.path.join(data_dir,'train/images')
-lbl_dir = os.path.join(data_dir,'train/labels')
-test_dir = os.path.join(data_dir,'train/test')
+trn_dir = os.path.join(data_dir,'train')
+tst_dir = os.path.join(data_dir,'test')
 
 #-- read in images
-def load_train_data()
+def load_train_data():
+    #-- get a list of the input files
+    trn_list = glob(os.path.join(trn_dir,'images/*.png'))
+    tst_list = glob(os.path.join(tst_dir,'images/*.png'))
+    #-- get just the file names
+    trn_files = [os.path.basename(i) for i in trn_list]
+    tst_files = [os.path.basename(i) for i in tst_list]
 
+    #-- read training data
+    n = len(trn_files)
+    #-- get dimensions, force to 1 b/w channel
+    w,h = np.array(Image.open(trn_list[0]).convert('L')).shape
 
-def main()
+    train_img = np.zeros((n,h,w,1))
+    train_lbl = np.zeros((n,w,h,1))
+    for i,f in enumerate(trn_files):
+        #-- same file name but different directories for images and labels
+        train_img[i,:,:,0] = np.array(Image.open(os.path.join(trn_dir,'images',f)).convert('L'))/255.
+        train_lbl[i,:,:,0] = np.array(Image.open(os.path.join(trn_dir,'labels',f)).convert('L'))/255.
+
+    #-- also get the test data
+    n_test = len(tst_files)
+    #-- get dimensions, force to 1 b/w channel
+    w_test,h_test = np.array(Image.open(tst_list[0]).convert('L')).shape
+    test_img = np.zeros((n_test,h_test,w_test,1))
+    for i in range(n_test):
+        test_img[i,:,:,0] = np.array(Image.open(tst_list[i]).convert('L'))/255.
+
+    return {'trn_img':train_img,'trn_lbl':train_lbl,'tst_img':test_img,\
+        'trn_names':trn_files,'tst_names':tst_files}
+
+def main():
     #-- load images
-    train_img,train_lbl,test_img = load_train_data()
+    data = load_train_data()
+    train_img = data['trn_img']
+    train_lbl = data['trn_lbl']
+    test_img = data['tst_img']
+
+    n,height,width,channels=train_img.shape
+    print('width=%i'%width)
+    print('height=%i'%height)
 
     #-- import mod
-    unet = imp.load_source('unet_model', os.path.join(ddir,'frontlearn_unet.py')
-    model = unet()
-    #-- create checkpoint
-    model_checkpoint = keras.callbacks.ModelCheckpoint('frontlearn_weights.h5', monitor='loss',\
-        verbose=1, save_best_only=True)
-    #-- now fit the model
-    model.fit(train_img, train_lbl, batch_size=5, nb_epoch=10, verbose=1,\
-        validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+    unet = imp.load_source('unet_model', os.path.join(ddir,'frontlearn_unet.py'))
+    model = unet.unet_model(height,width,channels)
 
-    #-- Now test the model
-    unet_test = model.predict(test_img, batch_size=1, verbose=1)
-    np.save(os.path.join(test_dir,'frontlearn_test.npy'),unet_test)
-    #-- also save the test image
-    out_imgs = np.load(os.path.join(test_dir,'frontlearn_test.npy'))
-    for i,m in enumerate(out_imgs):
-        im = keras.preprocessing.image.img_to_array(m)
-        im.save(os.path.join(test_dir,'%i.jpg'%i))
+    #-- checkpoint file
+    chk_file = os.path.join(ddir,'frontlearn_weights.h5')
+    #-- if file exists, just read model from file
+    if os.path.isfile(chk_file):
+        print('Check point exists; loading model from file.')
+        # load weights
+        model.load_weights(chk_file)
+        # Compile model (required to make predictions)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #-- if not train model
+    else:
+        print('Did not find check points. Training model...')
+        #-- create checkpoint
+        model_checkpoint = keras.callbacks.ModelCheckpoint(chk_file, monitor='loss',\
+            verbose=1, save_best_only=True)
+        #-- now fit the model
+        model.fit(train_img, train_lbl, batch_size=10, epochs=50, verbose=1,\
+            validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+
+    print('Model is trained. Running on test data...')
+
+    #-- Now test the model on both the test data and teh train data
+    out_imgs = model.predict(train_img, batch_size=1, verbose=1)
+    print out_imgs.shape
+    #-- save the test image
+    for i in range(len(out_imgs)):
+        im = image.array_to_img(out_imgs[i])
+        im.save(os.path.join(trn_dir,'output/%s'%data['trn_names'][i]))
+
+    out_imgs = model.predict(test_img, batch_size=1, verbose=1)
+    print out_imgs.shape
+    #-- save the test image
+    for i in range(len(out_imgs)):
+        im = image.array_to_img(out_imgs[i])
+        im.save(os.path.join(tst_dir,'output/%s'%data['tst_names'][i]))
 
 if __name__ == '__main__':
     main()
--- frontlearn_unet.py
+++ frontlearn_unet.py
@@ -12,21 +12,21 @@ Update History
 import keras.layers as kl
 import keras.models as km
 
-def unet_model(channels,height,width):
+def unet_model(height,width,channels):
 
     #-- define input
-    inputs = kl.Input((height,width,channels)
+    inputs = kl.Input((height,width,channels))
 
     #-- 1st convlution layer
-    #-- c1 has shape (None, w, h, 12), 12 convlution outputs for 12 filters
+    #-- c1 has shape (None, h, 2, 12), 12 convlution outputs for 12 filters
     c1 = kl.Conv2D(12,3,activation='relu',padding='same')(inputs)
 
     #-- pool, 2x2 blockcs
-    #-- p1 has shape (None, w/2, h/2, 12)
+    #-- p1 has shape (None h/2, w/2,  12)
     p1 = kl.MaxPooling2D(pool_size=(2,2))(c1)
 
     # 2nd convlution layer
-    #-- c2 has shape (None, w/2, h/2, 24)
+    #-- c2 has shape (None, h/2,w/2, 24)
     c2 = kl.Conv2D(24,3,activation='relu',padding='same')(p1)
 
     #---------------------------------------------
@@ -35,25 +35,25 @@ def unet_model(channels,height,width):
     #-- concatenate the 1st convolution layer with an upsampled 2nd layer
     #-- where the missing elements in the 2nd layer are padded with 0
     #-- concatenating along the color channels
-    #-- upsampled_c2 has shape (None, w, h ,24)
+    #-- upsampled_c2 has shape (None,h,w,24)
     upsampled_c2 = kl.UpSampling2D(size=(2,2))(c2)
-    #-- up1 has shape (None, w, h , 36)  (12+24 channels from c2 and c1)
+    #-- up1 has shape (None, h, w, 36)  (12+24 channels from c2 and c1)
     up1 = kl.concatenate([upsampled_c2,c1],axis=3)
     #-- now do a convlution with the merged upsampled layer
-    #-- c4 has shape (None, w, h , 12)
+    #-- c4 has shape (None, h, w, 12)
     c4 = kl.Conv2D(12,3,activation='relu',padding='same')(up1)
 
     #-- convlution across the last 12 filters into 3 channels
-    #-- c45has shape (None, w, h , 3)
+    #-- c45has shape (None,h,w,3)
     c5 = kl.Conv2D(3,3,activation='relu',padding='same')(c4)
 
-    #-- do one final sigmoid convolution into just 1 final channel (None, w, h , 1)
+    #-- do one final sigmoid convolution into just 1 final channel (None,h,w,1)
     c6 = kl.Conv2D(1,1,activation='sigmoid')(c5)
 
     #-- make model
     model = km.Model(input=inputs,output=c6)
     #-- compile model
-    model.compile(loss='categorical_crossentropy',optimizer='adam',\
+    model.compile(loss='binary_crossentropy',optimizer='adam',\
         metrics=['accuracy'])
 
     #-- return model
