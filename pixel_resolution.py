4cbf38b89880f7996947d5e3fc9f0608a2593b13
Yara Mohajerani <ymohajer@uci.edu>
2018-12-25 00:11:31 -0500

    Add successful cases only to pixel errors

--- pixel_resolution.py
+++ pixel_resolution.py
@@ -32,14 +32,30 @@ for glacier in glacier_list:
         print('y pixel size %f'%(dy/300.))
 
         if glacier == 'Helheim':
-            print('x error %f pixels'%(96.31/(dx/200.)))
-            print('y error %f pixels'%(96.31/(dy/300.)))
+            print('\n\nNN x error %f pixels'%(96.31/(dx/200.)))
+            print('NN y error %f pixels'%(96.31/(dy/300.)))
+
+            print('\nSobel x error %f pixels'%(836.35/(dx/200.)))
+            print('Sobel y error %f pixels'%(836.35/(dy/300.)))
+
+            print('\nManual x error %f pixels'%(92.49/(dx/200.)))
+            print('Manual y error %f pixels'%(92.49/(dy/300.)))
+
+            print('\nsuccess NN x error %f pixels'%(85.26125/(dx/200.)))
+            print('success NN y error %f pixels'%(85.26125/(dy/300.)))
+
+            print('\nsuccess Sobel x error %f pixels'%(193.01875/(dx/200.)))
+            print('success Sobel y error %f pixels'%(193.01875/(dy/300.)))
+
+            print('\nsuccess Manual x error %f pixels'%(89.095/(dx/200.)))
+            print('success Manual y error %f pixels'%(89.095/(dy/300.)))
+
         elif glacier == 'Sverdrup':
-            print('x error %f pixels'%(137.61/(dx/200.)))
+            print('\n\nx error %f pixels'%(137.61/(dx/200.)))
             print('y error %f pixels'%(137.61/(dy/300.)))
         elif glacier == 'Kangerlussuaq':
             # 175.90 with 500m buffer but edges are mismatched because of weird boundary
             # so 700 buffer might be a fairer comparison
-            print('x error %f pixels'%(124.40/(dx/200.))) 
+            print('\n\nx error %f pixels'%(124.40/(dx/200.))) 
             print('y error %f pixels'%(124.40/(dy/300.)))
         print('\n')        
