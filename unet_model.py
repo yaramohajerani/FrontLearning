dda732e61023900ade600492ebc1b1152e1add92
ymohajer <ymohajer@uci.edu>
2019-01-11 16:54:07 -0800

    fix batch normalization axis input

--- unet_model.py
+++ unet_model.py
@@ -1,18 +1,19 @@
 #!/anaconda2/bin/python2.7
 u"""
 frontlearn_unet.py
-by Yara Mohajerani (Last Update 09/2018)
+by Yara Mohajerani (Last Update 01/2019)
 
 Construct a dynamic u-net model with a variable
 number of layers for glacier calving front detection.
 
 Update History
-        09/2018 Add multiple functions to test different versions
-                Don't compile (compile in main script to allow for 
-                different weighting experiments)
-                Add multiple functions with different architectures
-                Add new option for batch normalization instead of dropout
-        04/2018 Written
+    01/2019 Fix batch normalization axis input
+    09/2018 Add multiple functions to test different versions
+            Don't compile (compile in main script to allow for 
+            different weighting experiments)
+            Add multiple functions with different architectures
+            Add new option for batch normalization instead of dropout
+    04/2018 Written
 """
 from keras import backend as K
 import keras.layers as kl
@@ -182,8 +183,8 @@ def unet_model_linear_normalized(height=0,width=0,channels=1,n_init=12,n_layers=
     p[0] = inputs
     for i in range(1,n_layers+1):
         #-- convolution layer
-        c[i] = BatchNormalization()(kl.Conv2D(n_init*i,3,activation='relu',padding='same')(p[i-1]))
-        c[i] = BatchNormalization()(kl.Conv2D(n_init*i,3,activation='relu',padding='same')(c[i]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_init*i,3,activation='relu',padding='same')(p[i-1]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_init*i,3,activation='relu',padding='same')(c[i]))
 
         #-- pool, 2x2 blockcs
         #-- don't do pooling for the last down layer
@@ -201,22 +202,22 @@ def unet_model_linear_normalized(height=0,width=0,channels=1,n_init=12,n_layers=
         #-- concatenate the 1st convolution layer with an upsampled 2nd layer
         #-- where the missing elements in the 2nd layer are padded with 0
         #-- concatenating along the color channels
-        upsampled_c[i] = BatchNormalization()(kl.UpSampling2D(size=(2,2))(c[count]))
+        upsampled_c[i] = kl.UpSampling2D(size=(2,2))(c[count])
         up[i] = kl.concatenate([upsampled_c[i],c[count-1]],axis=3)
         #-- now do a convolution with the merged upsampled layer
         i += 1
-        c[i] = BatchNormalization()(kl.Conv2D(n_init*(count-1),3,activation='relu',padding='same')(up[i-1]))
-        c[i] = BatchNormalization()(kl.Conv2D(n_init*(count-1),3,activation='relu',padding='same')(c[i]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_init*(count-1),3,activation='relu',padding='same')(up[i-1]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_init*(count-1),3,activation='relu',padding='same')(c[i]))
         #-- counter decreases as we go back up
         count -= 1
 
     print('Number of Convlution Filters at the end of up segment: ',n_init*count)
     #-- convlution across the last n_iniy filters into 3 channels
     i += 1
-    c[i] = BatchNormalization()(kl.Conv2D(3,3,activation='relu',padding='same')(c[i-1]))
+    c[i] = BatchNormalization(axis=-1)(kl.Conv2D(3,3,activation='relu',padding='same')(c[i-1]))
     #-- do one final sigmoid convolution into just 1 final channel (None,h,w,1)
     i += 1
-    c[i] = BatchNormalization()(kl.Conv2D(1,1,activation='sigmoid')(c[i-1]))
+    c[i] = BatchNormalization(axis=-1)(kl.Conv2D(1,1,activation='sigmoid')(c[i-1]))
     #-- reshape into a flattened output to match sample weights
     i += 1
     c[i] = kl.Reshape((height*width,1,))(c[i-1])
@@ -247,8 +248,8 @@ def unet_model_double_normalized(height=0,width=0,channels=1,n_init=12,n_layers=
     n_filts = copy.copy(n_init)
     for i in range(1,n_layers+1):
         #-- convolution layer
-        c[i] = BatchNormalization()(kl.Conv2D(n_filts,3,activation='relu',padding='same')(p[i-1]))
-        c[i] = BatchNormalization()(kl.Conv2D(n_filts,3,activation='relu',padding='same')(c[i]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_filts,3,activation='relu',padding='same')(p[i-1]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_filts,3,activation='relu',padding='same')(c[i]))
 
         #-- pool, 2x2 blockcs
         #-- don't do pooling for the last down layer
@@ -269,22 +270,22 @@ def unet_model_double_normalized(height=0,width=0,channels=1,n_init=12,n_layers=
         #-- concatenate the 1st convolution layer with an upsampled 2nd layer
         #-- where the missing elements in the 2nd layer are padded with 0
         #-- concatenating along the color channels
-        upsampled_c[i] = BatchNormalization()(kl.UpSampling2D(size=(2,2))(c[count]))
+        upsampled_c[i] = kl.UpSampling2D(size=(2,2))(c[count])
         up[i] = kl.concatenate([upsampled_c[i],c[count-1]],axis=3)
         #-- now do a convlution with the merged upsampled layer
         i += 1
-        c[i] = BatchNormalization()(kl.Conv2D(n_filts,3,activation='relu',padding='same')(up[i-1]))
-        c[i] = BatchNormalization()(kl.Conv2D(n_filts,3,activation='relu',padding='same')(c[i]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_filts,3,activation='relu',padding='same')(up[i-1]))
+        c[i] = BatchNormalization(axis=-1)(kl.Conv2D(n_filts,3,activation='relu',padding='same')(c[i]))
         #-- counter decreases as we go back up
         count -= 1
 
     print('Number of Convlution Filters at the end of up segment: ',n_filts)
     #-- convlution across the last n_iniy filters into 3 channels
     i += 1
-    c[i] = BatchNormalization()(kl.Conv2D(3,3,activation='relu',padding='same')(c[i-1]))
+    c[i] = BatchNormalization(axis=-1)(kl.Conv2D(3,3,activation='relu',padding='same')(c[i-1]))
     #-- do one final sigmoid convolution into just 1 final channel (None,h,w,1)
     i += 1
-    c[i] = BatchNormalization()(kl.Conv2D(1,1,activation='sigmoid')(c[i-1]))
+    c[i] = BatchNormalization(axis=-1)(kl.Conv2D(1,1,activation='sigmoid')(c[i-1]))
     #-- reshape into a flattened output to match sample weights
     i += 1
     c[i] = kl.Reshape((height*width,1,))(c[i-1])
